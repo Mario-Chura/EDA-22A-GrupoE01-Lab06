298186b72922ddd7145b3829b4f92eb1b49a0941
Mario-Chura <mchurapum@unsa.edu.pe>
2022-08-06 01:20:48 -0500

    Node

--- Ejercicio 1/BTree.java
+++ Ejercicio 1/BTree.java
@@ -6,5 +6,16 @@ public class BTree<Key extends Comparable<Key>, Value>  {
 	private Node root;       // raiz del arbol B
 	private int height;      // altura del arbol B
 	private int n;           // Numero de los pares clave-valor del arbol B
-    
+
+    // Tipo de dato del nodo auxiliar del arbol B
+	private static final class Node {
+		private int m;                             // Numero de hijos
+		private Entry[] children = new Entry[M];   // Array de hijos
+
+		// Creamos un nodo con "K" hijos
+		private Node(int k) {
+			m = k;
+		}
+	}
+
 }
